0e5b9a55abea856a195a5da9ad48568d47e6d283
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-05-22 19:44:18 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
 
     userId = sys.argv[1]
-    user = requests.aget("https://jsonplaceholder.typicode.com/users/{}"
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(userId))
 
     name = user.json().get('name')
